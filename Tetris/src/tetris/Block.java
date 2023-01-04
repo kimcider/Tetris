cc5655ef042db9f5ca225e76e70aafb2b19e73ee
kimcider <105146508+kimcider@users.noreply.github.com>
2023-01-04 16:46:28 +0900

    23/01/04

---
+++ Tetris/src/tetris/Block.java
@@ -0,0 +1,11 @@
+package tetris;
+
+import java.awt.*;
+import javax.swing.*;
+public class Block extends JPanel{
+	public Block() {
+		setSize(50,50);
+		setBackground(Color.blue);
+		setVisible(true);
+	}
+}
